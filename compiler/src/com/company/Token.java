a1b000a80ed59445adc382a7241d6f97202c1f29
Fatmanur Özdemir <ozdemir.fatmanur18@gmail.com>
2021-04-23 14:06:09 +0300

    lexeme operation is updated

--- compiler/src/com/company/Token.java
+++ compiler/src/com/company/Token.java
@@ -1,21 +1,137 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Token {
-    String lexeme;
-    String tokenClass;
 
-    Token(){
+    private List<String> keywords;
+    private List<String> operators;
+
+    public Token()  {
+        this.keywords = createKeywords();
+        this.operators = createOperators();
     }
 
+    public List<String> createKeywords(){
 
-    Token(String tokenClass, String lexeme){
-        this.tokenClass = tokenClass;
-        this.lexeme = lexeme;
+        List<String> keyword = new ArrayList<>();
+        keyword.add("if");
+        keyword.add("else if");
+        keyword.add("else");
+        keyword.add("int");
+        keyword.add("short");
+        keyword.add("double");
+        keyword.add("string");
+        keyword.add("double");
+        keyword.add("for");
+        keyword.add("while");
 
+        return keyword;
     }
 
+    public List<String> createOperators(){
 
+        List<String> operators = new ArrayList<>();
+        operators.add("+");
+        operators.add("/");
+        operators.add("*");
+        operators.add("-");
+        operators.add("%");
+        operators.add("(");
+        operators.add(")");
+        operators.add("{");
+        operators.add("}");
+        operators.add("[");
+        operators.add("]");
+        operators.add(",");
+        operators.add("||");
+        operators.add("&&");
 
+        return operators;
 
+    }
+    public String getToken(String word){
+
+        if(keywords.contains(word)){
+            return("keyword");
+        }else if(operators.contains(word)){
+            return("operator");
+        }else if(isNumeric(word)){
+            return("number");
+        }else if(word.equals("<")){
+            return("less_op");
+        }else if(word.equals("<=")){
+            return("le_op");
+        }else if(word.equals("=")){
+            return("assign_op");
+        }else if(word.equals("<>")){
+            return("not_op");
+        }else if(word.equals(">")){
+            return("g_op");
+        }else if(word.equals(">=")){
+            return("ge_op");
+        }else if(word.equals("==")){
+            return("is_equal_op");
+        }else if(word.equals("!=")){
+            return("is_not_equal_op");
+        }else if(word.equals(";")){
+            return("end_op");
+        }else if(isId(word)){
+            return("id");
+        }else{
+            return("not identified");
+        }
+    }
+
+    public static boolean isNumeric(String string) {
+        int intValue;
+
+        if(string == null || string.equals("")) {
+            System.out.println(string + "String cannot be parsed, it is null or empty.");
+            return false;
+        }
+
+        try {
+            intValue = Integer.parseInt(string);
+            return true;
+        } catch (NumberFormatException e) {
+
+        }
+        return false;
+    }
+
+    public static boolean isId(String string) {
+
+        if(string == null || string.equals("")) {
+            System.out.println(string + "String cannot be parsed, it is null or empty.");
+            return false;
+        }
+
+        if( string.length() > 64 ){
+            System.out.println("Error for " + string + " ! Identifier cannot be longer than 64.");
+            return false;
+        }
+
+        int index = 0;
+        if(Character.isDigit(string.charAt(0))) {
+            System.out.println("Error for " + string + " ! Identifier cannot start with digit.");
+            return false;
+        }
+        if(string.charAt(0) == '$' || string.charAt(0) == '_'){
+            if (string.length()==1){
+                System.out.println("Error for " + string + " ! Identifier cannot be just '$' or '_'.");
+                return false;
+            }
+            index = 1;
+        }
+        for (int i = index; i < string.length(); i++) {
+            if( !(Character.isDigit(string.charAt(i))) && !(Character.isLetter(string.charAt(i))) ) {
+                System.out.println("Error for " + string + " ! Identifier must contain only letter or digit except first character.");
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
